e54ca019b20fc4ba8c60109cc7a21f79a31cb767
csoke.balazs <csoke.balazs@117-13>
2024-04-17 09:38:49 +0200

    Add project files.

---
+++ termek_cskbalazs/Termek.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace termek_cskbalazs
+{
+    internal class Termek
+    {
+        string cikkszam;
+        public string megnevezes;
+        public int beszerzesiar;
+        int afakulcs;
+        bool akcios;
+        int kedvezmény;
+        
+        public netto()
+        {
+            double netto = Math.Round(beszerzesiar * 1.35, 2);
+        }
+    }
+}
